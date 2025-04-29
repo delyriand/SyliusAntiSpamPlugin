0aaf657b81ce14618af2730dc2280ef975d95aea
Maxime Leclercq <m.leclercq@monsieurbiz.com>
2025-04-29 14:07:40 +0200

    fix: avoid error cannot have an empty name for command

--- src/Command/RemoveExceededQuarantineItemsCommand.php
+++ src/Command/RemoveExceededQuarantineItemsCommand.php
@@ -14,14 +14,17 @@ declare(strict_types=1);
 namespace MonsieurBiz\SyliusAntiSpamPlugin\Command;
 
 use MonsieurBiz\SyliusAntiSpamPlugin\Remover\ExceededQuarantineItemsRemoverInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'monsieurbiz:anti-spam:remove-exceeded-quarantine-items',
+    description: 'Remove exceeded quarantine items',
+)]
 final class RemoveExceededQuarantineItemsCommand extends Command
 {
-    protected static $defaultName = 'monsieurbiz:anti-spam:remove-exceeded-quarantine-items';
-
     private ExceededQuarantineItemsRemoverInterface $exceededQuarantineItemsRemover;
 
     public function __construct(
@@ -39,6 +42,6 @@ final class RemoveExceededQuarantineItemsCommand extends Command
     {
         $this->exceededQuarantineItemsRemover->remove();
 
-        return 0; // TODO o be replaced by Command::SUCCESS when the supported sylius version is higher than 1.12.
+        return Command::SUCCESS;
     }
 }
